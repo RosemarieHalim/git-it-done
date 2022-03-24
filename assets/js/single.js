7e61a39175f76f83be2e0dda14b0d509f190d446
Rosemarie <rosemarie.m.halim@gmail.com>
2022-03-23 23:52:06 -0400

    creating query parameters

--- assets/js/single.js
+++ assets/js/single.js
@@ -6,23 +6,23 @@ var getRepoIssues = function(repo) {
 
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
-            response.json().then(function(data) {
-              displayIssues(data);
-          
-              // check if api has paginated issues
-              if (response.headers.get("Link")) {
-                displayWarning(repo);
-              }
-            });
-        }
-        else {
-            console.log(response);
-            alert("There was a problem with your request!");
-        }
-    });
+        response.json().then(function(data) {
+            displayIssues(data);
+    
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+            displayWarning(repo);
+            }
+        });
+        } else {
+        // if not successful, redirect to homepage
+        document.location.replace("./index.html");
+    }
+  });
 };
 
 var displayIssues = function(issues) {
@@ -73,4 +73,18 @@ var displayWarning = function(repo) {
     limitWarningEl.appendChild(linkEl);
 };
 
-getRepoIssues("facebook/react");
+var getRepoName = function() {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+  
+    if (repoName) {
+      // display repo name on the page
+      repoNameEl.textContent = repoName;
+  
+      getRepoIssues(repoName);
+    } else {
+      // if no repo was given, redirect to the homepage
+      document.location.replace("./index.html");
+    }
+};
